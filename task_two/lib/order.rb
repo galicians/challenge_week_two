96d44616f4325131f36373b231065bfebe74ac42
galicians <galicians@gmail.com>
2014-09-27 13:16:04 +0100

    testing order class

--- task_two/lib/order.rb
+++ task_two/lib/order.rb
@@ -1,4 +1,20 @@
 class Order
 
+	attr_accessor :list
 
+	def initialize
+		@list = {} #when should be using a hash here
+	end
+
+	def add_lineitem(lineitem)
+		@list[lineitem.dish] = lineitem.quantity
+	end
+
+	def remove_lineitem(lineitem)
+		@list.delete(lineitem.dish)
+	end
+
+	def change_quantity(lineitem,4)
+		
+	end
 end
--- task_two/spec/order_spec.rb
+++ task_two/spec/order_spec.rb
@@ -1,21 +1,35 @@
 require 'order'
 
 describe Order do
+	let(:dish) {double :dish, name: :paella, price: 12}
+	let(:lineitem) { double :lineitem, dish: dish, quantity: 3}
+	let(:order) { Order.new }
 
-	it "should accept lineitems" do
-	
+	it "should be empty when initialized" do
+		expect(order.list).to eq({})
 	end
 
-	it "should allow to to delete a lineitem from it" do
+	it "should accept lineitems" do
+		order.add_lineitem(lineitem)
+		expect(order.list.size).to eq 1
+	end
 
+	it "should allow to to delete a specific lineitem from it" do
+		order.add_lineitem(lineitem)
+		order.remove_lineitem(lineitem)
+		expect(order.list.size).to eq 0
 	end
 
 	it "should allow to change the quantity of a specific lineitem" do
-
+		expect(lineitem.quantity).to eq 3
+		order.change_quantity(lineitem)
 	end
 
 	it "should allow to change the dish of a specific lineitem" do
 
+	end
+
+	it "should have a total" do
 	end
 
 end
