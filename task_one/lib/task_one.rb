7d1f925624ce687be0baea69bd88d4d80bbf0761
galicians <galicians@gmail.com>
2014-09-29 20:31:34 +0100

    changing the inject iterators method

--- task_one/lib/task_one.rb
+++ task_one/lib/task_one.rb
@@ -1,16 +1,22 @@
 class Array
 
-	def inject_using_iterators(memo=0,&blk)
-		array = self
+	def inject_iterators(memo=0,&blk)
+		array = self.clone
 		if memo == 0
-			memo = self.first
+			memo = first
 			array.shift
 		end
 		array.each do |x|
 			memo = yield memo,x
-			puts memo
 		end
 		memo
+	end
+
+	def inject_recursion(memo=0,&blk)
+		return memo if empty?
+		memo = yield memo,first
+		shift
+		inject_recursion(memo,&blk)
 	end
 
 end
